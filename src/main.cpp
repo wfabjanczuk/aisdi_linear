9f483de7d1779264942b7ed52bce8d625e478af2
wfabjanc <wojciech.fabjanczuk@aster.org.pl>
2018-12-14 05:11:35 +0100

    Performance tests

--- src/main.cpp
+++ src/main.cpp
@@ -1,28 +1,108 @@
 #include <cstddef>
 #include <cstdlib>
+#include <iostream>
 #include <string>
+#include <chrono>
 
 #include "Vector.h"
 #include "LinkedList.h"
 
+using namespace std;
+
+#define prependLabel "PREPEND PERFORMANCE TEST"
+#define popFirstLabel "POP FIRST PERFORMANCE TEST"
+#define copyLabel "COPY PERFORMANCE TEST"
+
 namespace
 {
 
-template <typename T>
-using LinearCollection = aisdi::LinkedList<T>;
+    template<typename T>
+    using Vector = aisdi::Vector<T>;
 
-void perfomTest()
-{
-  LinearCollection<std::string> collection;
-  collection.append("TODO");
+    template<typename T>
+    using LinkedList = aisdi::LinkedList<T>;
+
 }
 
-} // namespace
+void VectorPrepend(Vector<int>& vector) {
+    vector.prepend(1);
+}
 
-int main(int argc, char** argv)
-{
-  const std::size_t repeatCount = argc > 1 ? std::atoll(argv[1]) : 10000;
-  for (std::size_t i = 0; i < repeatCount; ++i)
-    perfomTest();
-  return 0;
+void ListPrepend(LinkedList<int>& list) {
+    list.prepend(1);
+}
+
+void VectorPopFirst(Vector<int>& vector) {
+    vector.popFirst();
+}
+
+void ListPopFirst(LinkedList<int>& list) {
+    list.popFirst();
+}
+
+void VectorCopy(Vector<int>& vector) {
+    Vector<int> new_vector;
+    new_vector = vector;
+}
+
+void ListCopy(LinkedList<int>& list) {
+    LinkedList<int> new_list;
+    new_list = list;
+}
+
+void test(
+        int collection_size,
+        int repeat_count,
+        string label,
+        void (*vectorTest)(Vector<int>&),
+        void (*listTest)(LinkedList<int>&)) {
+    Vector<int> vector;
+    LinkedList<int> list;
+
+    for (int i = 0; i < collection_size; i++) {
+        vector.append(i);
+        list.append(i);
+    }
+    cout << "\n>> " << label << "\n\n";
+    cout << "> Collection size N=" << collection_size << endl;
+    cout << "> Repeat count k=" << repeat_count << endl;
+
+    auto start = chrono::high_resolution_clock::now();
+    for (int i = 0; i < repeat_count; i++) {
+        vectorTest(vector);
+    }
+    auto finish = chrono::high_resolution_clock::now();
+    chrono::duration<double> elapsed = finish - start;
+    cout << "> Vector::prepend elapsed time: " << elapsed.count() << " s\n";
+
+    start = chrono::high_resolution_clock::now();
+    for (int i = 0; i < repeat_count; i++) {
+        listTest(list);
+    }
+    finish = chrono::high_resolution_clock::now();
+    elapsed = finish - start;
+    cout << ">   List::prepend elapsed time: " << elapsed.count() << " s\n\n";
+}
+
+int main() {
+
+    test(1000, 5, prependLabel, &VectorPrepend, &ListPrepend);
+    test(10000, 5, prependLabel, &VectorPrepend, &ListPrepend);
+    test(100000, 5, prependLabel, &VectorPrepend, &ListPrepend);
+    test(1000000, 5, prependLabel, &VectorPrepend, &ListPrepend);
+    test(10000000, 5, prependLabel, &VectorPrepend, &ListPrepend);
+
+    test(1000, 5, popFirstLabel, &VectorPopFirst, &ListPopFirst);
+    test(10000, 5, popFirstLabel, &VectorPopFirst, &ListPopFirst);
+    test(100000, 5, popFirstLabel, &VectorPopFirst, &ListPopFirst);
+    test(1000000, 5, popFirstLabel, &VectorPopFirst, &ListPopFirst);
+    test(10000000, 5, popFirstLabel, &VectorPopFirst, &ListPopFirst);
+
+    test(1000, 1, copyLabel, &VectorCopy, &ListCopy);
+    test(10000, 1, copyLabel, &VectorCopy, &ListCopy);
+    test(100000, 1, copyLabel, &VectorCopy, &ListCopy);
+    test(1000000, 1, copyLabel, &VectorCopy, &ListCopy);
+    test(10000000, 1, copyLabel, &VectorCopy, &ListCopy);
+
+    return 0;
 }
